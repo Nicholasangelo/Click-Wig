c2061db9db0ec1f2f38a5f89638f4a1fd38d07be
Nicholas Angelo <40876154+Nicholasangelo@users.noreply.github.com>
2018-12-22 23:53:24 -0500

    submitting and deployng

---
+++ click-wig/build/precache-manifest.f89563193173f1aeeb8745830f521407.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d31f08a2f1a90bdf5310",
+    "url": "/click-wig/static/js/runtime~main.d31f08a2.js"
+  },
+  {
+    "revision": "d9fa9070632e1c7855a6",
+    "url": "/click-wig/static/js/main.d9fa9070.chunk.js"
+  },
+  {
+    "revision": "46b1084bfdcac49c9b0e",
+    "url": "/click-wig/static/js/1.46b1084b.chunk.js"
+  },
+  {
+    "revision": "d9fa9070632e1c7855a6",
+    "url": "/click-wig/static/css/main.8166a07b.chunk.css"
+  },
+  {
+    "revision": "b626dc86069e1ac78060e6025144121a",
+    "url": "/click-wig/index.html"
+  }
+];
--- click-wig/src/components/Header/index.js
+++ click-wig/src/components/Header/index.js
@@ -4,7 +4,8 @@ import "./Header.css";
 function Header(props) {
     return (
         <div className="Header">
-        <h2 className="title">Click Wig</h2>
+        <h1 className="title">Nick Through the Ages</h1>
+        <h3>I've tried out a lot of looks through the ages, can you keep them sorted?</h3>
         <p>Click a card to get started.  Click one you've already clicked and you're finished!</p>
         <div className="scoreBoard">
         Score: {props.score} Top Score: {props.topScore}
